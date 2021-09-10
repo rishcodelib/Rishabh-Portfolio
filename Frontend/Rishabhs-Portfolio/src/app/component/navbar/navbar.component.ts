947ca5f2f5933ad822efd3f5c29d7677aaf23cad
rishcodelib <ri.sbh23@gmail.com>
2021-09-10 17:17:21 +0530

    Changes in navbar Component

--- Frontend/Rishabhs-Portfolio/src/app/component/navbar/navbar.component.ts
+++ Frontend/Rishabhs-Portfolio/src/app/component/navbar/navbar.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  @HostListener('window:scroll', ['$event']) onWindowScroll($event) {
+    const element = document.querySelector('.navbar') as HTMLElement;
+    if (window.pageYOffset > element.clientHeight) {
+      element.classList.remove('bg-transparent');
+      element.classList.remove('navbar-dark');
+      element.classList.add('bg-light');
+      element.classList.add('navbar-light');
+      console.log(element);
+    } else {
+      element.classList.add('bg-transparent');
+      element.classList.add('navbar-dark');
+      element.classList.remove('bg-light');
+      element.classList.remove('navbar-light');
+    }
   }
-
 }
