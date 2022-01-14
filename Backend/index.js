b276b7252302583d931ed364c7befff80cb79cb2
rishcodelib <ri.sbh23@gmail.com>
2022-01-14 16:17:58 +0530

    using master branch

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const { MongoClient } = require('mongodb')
 const db = require('dotenv');
-const { connectMongoDB } = require('./config/mongodb');
+// const { connectMongoDB } = require('./config/mongodb');
 const app = express();
 const SERVER_PORT = 3000
 
 // Connecting to Mongo DB
-connectMongoDB()
+// connectMongoDB()
 
 //Built-in middleware
 app.use(express.json())
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
 })
 
 //Routes
-app.use('/api/user',userRoutes)
+// app.use('/api/user',userRoutes)
 
 app.listen(SERVER_PORT, () => {
   console.log(`App is listening at port ${SERVER_PORT}`)
