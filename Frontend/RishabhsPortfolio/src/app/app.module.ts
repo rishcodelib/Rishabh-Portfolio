fe8d9a6aa1619f953dcfa7a212bc487b711a1273
rishcodelib <ri.sbh23@gmail.com>
2022-01-14 22:30:35 +0530

    added components

--- Frontend/RishabhsPortfolio/src/app/app.module.ts
+++ Frontend/RishabhsPortfolio/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { AboutComponent } from './component/about/about.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HeroimageComponent } from './component/heroimage/heroimage.component';
+import { SigninComponent } from './component/signin/signin.component';
+import { SignupComponent } from './component/signup/signup.component';
+import { ProjectsComponent } from './component/projects/projects.component';
+import { ProjectCarouselComponent } from './component/project-carousel/project-carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent,
+    HeroimageComponent,
+    SigninComponent,
+    SignupComponent,
+    ProjectsComponent,
+    ProjectCarouselComponent
   ],
   imports: [
     BrowserModule,
---
+++ Frontend/RishabhsPortfolio/src/app/component/project-carousel/project-carousel.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProjectCarouselComponent } from './project-carousel.component';
+
+describe('ProjectCarouselComponent', () => {
+  let component: ProjectCarouselComponent;
+  let fixture: ComponentFixture<ProjectCarouselComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProjectCarouselComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProjectCarouselComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
