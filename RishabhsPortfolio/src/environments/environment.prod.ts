facf8ea12848b78c7bfa49bebc86657185b61779
rishcodelib <ri.sbh23@gmail.com>
2024-05-12 22:28:02 +0530

    git manual versioning 0.1.0

--- RishabhsPortfolio/src/environments/environment.prod.ts
+++ RishabhsPortfolio/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   backendApi: `http://localhost:4000`,
-  frontendUrl: `http://localhost:4200`
+  frontendUrl: `http://localhost:4200`,
+  VERSION: '0.1.0'
   
 };
