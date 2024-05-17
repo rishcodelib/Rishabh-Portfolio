1e31b80512055bcc9e0711c5f1be617eeaef4468
rishcodelib <ri.sbh23@gmail.com>
2024-05-17 12:18:06 +0530

    updated version 25.5.2

--- RishabhsPortfolio/src/environments/environment.development.ts
+++ RishabhsPortfolio/src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   backendApi: `http://localhost:4000`,
   frontendUrl: `http://localhost:4200`,
-  VERSION: '24.5.1',
+  VERSION: '24.5.2',
   PROTOCOL:'https://',
   DOMAIN_NAME:'rishabhaggarwal.in',
   PORT:'8080',
--- RishabhsPortfolio/src/environments/environment.prod.ts
+++ RishabhsPortfolio/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: true,
   backendApi: `http://localhost:4000`,
   frontendUrl: `http://localhost:4200`,
-  VERSION: '24.5.1',
+  VERSION: '24.5.2',
   PROTOCOL:'https://',
   DOMAIN_NAME:'rishabhaggarwal.in',
   PORT:'8080',
