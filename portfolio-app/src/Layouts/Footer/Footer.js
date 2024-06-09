4a2155c27360e88dde25628b41d4bafdc6ad9b78
rishcodelib <ri.sbh23@gmail.com>
2024-06-09 12:55:46 +0530

    VERSION 2.0 REACT

---
+++ portfolio-app/src/Layouts/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return <footer class="myfooter mt-4">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="container content text-center">
+          <p class="display-7"> Portfolio By Rishabh Aggarwal | India | VERSION : 0.1.1 </p>
+            {/* <!-- <a  href="/signin"> Signin</a> */}
+            {/* | <a href="/signup"> Signup</a> </p> --> */}
+        </div>
+      </div>
+    </div>
+  </footer>
+}
+
+export default Footer;
